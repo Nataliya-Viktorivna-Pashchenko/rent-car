b35d70b78d0f82988ff508b00a1bde23e1792ba8
Nataliia Pashchenko <nvpashchenko@ukr.net>
2024-04-10 11:46:00 +0200

    fix: add basename

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/rent-car/'>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <App />
